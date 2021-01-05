51321f27ceae5b9e2918caf605e6acb51151a9b8
malik <malik.h@webdevpro.net>
2021-01-05 17:20:25 +0100

    enoncé

--- app-music/src/app/reactive-form-suite/reactive-form-suite.component.ts
+++ app-music/src/app/reactive-form-suite/reactive-form-suite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl , Validators , FormArray , FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-suite',
@@ -7,7 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormSuiteComponent implements OnInit {
 
-  constructor() { }
+  form2 ;
+
+  constructor( frmBuild : FormBuilder) {
+    this.form2 = frmBuild.group({ // new FormGroup({})
+      nom : ["valeur par défaut", Validators.required ]  ,   // nom : new FormControl()
+      images : frmBuild.array([ 
+        ["image 1"],
+        ["image 2"],
+        frmBuild.control("image 3"),
+      ]) //  new FormArray([])
+    });
+  }
+  onSubmit2(){
+    console.log(this.form2.value);
+  }
+
+  /*
+    form2 = new FormGroup({
+      nom : new FormControl( "valeur par défaut", Validators.required ),
+      images : new FormArray([])
+    })
+  */
+
+  form = new FormGroup({
+    categories : new FormArray([]) , // style de champ de formulaire ,
+    matieres : new FormArray([])
+  });
+
+  onKeyDownAdd(input){
+    // console.log(input.value); 
+    (this.form.get("categories") as FormArray ).push( new FormControl(input.value) )
+    input.value = ""; // vide le champ
+  }
+
+  onClickAdd(){
+    (this.form.get("matieres") as FormArray ).push( new FormControl() )
+  }
+
+  onSubmit(){
+    console.log(this.form.value)
+  }
+
+  
 
   ngOnInit(): void {
   }
