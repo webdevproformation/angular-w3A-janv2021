43b8d983dfc75c05e3f498109ee75a844d4a3cd7
malik <malik.h@webdevpro.net>
2021-01-05 13:01:55 +0100

    reactive form

--- app-music/src/app/reactive-form/reactive-form.component.ts
+++ app-music/src/app/reactive-form/reactive-form.component.ts
@@ -11,6 +11,7 @@ export class ReactiveFormComponent {
   // FormControl => item input / textarea dans notre formulaire 
   form = new FormGroup( { 
     nom : new FormControl("" , Validators.required ),
+    email : new FormControl( "", [ Validators.email , Validators.required ] ) ,
     commentaire : new FormControl(
                       "texte par défaut" , 
                         [ 
@@ -23,7 +24,18 @@ export class ReactiveFormComponent {
 
   // test : string = "valeur par défaut pour le champ test"
 
+  // ajouter un getter // éviter d'avoir à répéter form.get("email") dans la partie html 
+  get email(){
+    return this.form.get("email");
+  }
+  // créer des Validation personnalisée => ne veux pas qu'il y ait le symbole espace 
+  // validation async créer un compte => pseudo => (le pseudo que tu veux utiliser est réjà pris)
+  // erreur après la validation
+  // ... 
+
   onSubmit(){
-    console.log( this.form.value );
+    if(this.form.valid){
+      console.log( this.form.value );
+    }
   }
 }
