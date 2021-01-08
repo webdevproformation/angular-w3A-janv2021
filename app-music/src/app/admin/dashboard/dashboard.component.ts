ef90465e08cc371bdaf549cfbbdb6040aa5fcbcf
malik <malik.h@webdevpro.net>
2021-01-08 11:50:05 +0100

    correction

--- app-music/src/app/admin/dashboard/dashboard.component.ts
+++ app-music/src/app/admin/dashboard/dashboard.component.ts
@@ -2,11 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../../services/album';
 import { FirebaseService } from "../../services/firebase.service"
 import { Observable } from 'rxjs';
+import {trigger , style , transition , animate} from "@angular/animations";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations : [
+    trigger( "disparition" , [ 
+      transition("* => void", [ 
+        animate(2000 , style({ opacity : 0 , transform:"translateX(200px)" }))
+      ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
   albums : Observable<Array<Album>>
@@ -18,6 +26,10 @@ export class DashboardComponent implements OnInit {
 
   onClickDelete(id : string){
     this._firebasemusic.delete( id )
+  }
+
+  albumCheck(index, album){
+    return album ? album.id : undefined ;
   }
 
 }
