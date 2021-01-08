d51a449cd53afdf5995ad0dbccb1a0ebc837c5aa
malik <malik.h@webdevpro.net>
2021-01-08 17:17:22 +0100

    fin http

--- app-music/src/app/http/http.component.ts
+++ app-music/src/app/http/http.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http" ;
 import { map } from "rxjs/operators";
-import { Observable } from 'rxjs';
+import { Observable , merge } from 'rxjs';
+import { FormBuilder } from "@angular/forms"
 
 @Component({
   selector: 'app-http',
@@ -9,14 +10,19 @@ import { Observable } from 'rxjs';
   styleUrls: ['./http.component.scss']
 })
 export class HttpComponent implements OnInit {
-
+  form 
   url : string = "https://jsonplaceholder.typicode.com/posts"
-  articles$ : Observable<Array<{}>>
+  articles : any;
 
-  constructor( private _http : HttpClient) { }
+  constructor( private _http : HttpClient , private _formBuild : FormBuilder) { }
 
   ngOnInit(): void { 
-    this.articles$ = this._http.get( this.url ).pipe(
+    this.form = this._formBuild.group({
+      title : ["titre par défaut"],
+      body : ["contenu par défaut"]
+    })
+
+    this._http.get( this.url ).pipe(
       map( function( reponse : any ){ 
         return reponse.map( function(item){ 
           return {
@@ -31,7 +37,32 @@ export class HttpComponent implements OnInit {
           return item.id <= 10
         })
        })
-    )
+    ).subscribe( reponse => {
+      this.articles = reponse
+    })
+  }
+
+  onSubmitAdd(){
+    console.log(this.form.value)
+    this._http.post(this.url ,this.form.value).subscribe( reponse  => {
+      this.articles.unshift( reponse )
+    })
+    // vider le formulaire 
+    //this.form.reset();
+  }
+
+  onClickDelete(article){
+    this._http.delete(this.url +"/"+ article.id ).subscribe( reponse => {
+      if(reponse){
+        let articleAsupprimer = this.articles.find( function(item){
+          return item.id === article.id
+        });
+        let index = this.articles.indexOf(articleAsupprimer);
+        if(index !== -1){
+          this.articles.splice(index, 1)
+        }
+      }
+    })
   }
 
 }
